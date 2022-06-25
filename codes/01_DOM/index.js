86990d5fe288568c58f3d0e8c97c9a2d65ca10ba
jongkeun-kim <kjkandrea@gmail.com>
2022-06-25 15:29:38 +0900

    feat: add getElementById example

--- codes/01_DOM/index.js
+++ codes/01_DOM/index.js
@@ -1,10 +1,19 @@
 /**
- * DOM Tree 의 트리구조를 통하여 다음과 같이 h1 에 접근할 수 있다.
+ * 1.
+ * DOM Element.children 으로 요소 취득
  *
  * @children-1depth [html]
  * @children-2depth [head, body]
  * @children-3depth [h1, ...]
  */
 
-const h1 = document.children[0].children[1].children[0]
-console.log('1 : ', h1)
+const childElementNode = document.children[0].children[1].children[0]
+console.log('1. : ', childElementNode)
+
+/**
+ * 2.
+ * DOM getElementById 로 요소 취득
+ */
+
+const elementByIdHello = document.getElementById('hello')
+console.log('2. : ', elementByIdHello)
