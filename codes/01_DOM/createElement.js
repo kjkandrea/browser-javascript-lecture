a88f6c0d3dfbae1a7d05ab6d01ef99b15f020b3e
jongkeun-kim <kjkandrea@gmail.com>
2022-06-25 15:40:16 +0900

    feat: add textContent

--- codes/01_DOM/createElement.js
+++ codes/01_DOM/createElement.js
@@ -3,5 +3,14 @@
  * DOM createElement : ElementNode 생성
  */
 
-const paragraphElementNode = document.createElement('p')
-console.log('1. : ', paragraphElementNode)
+const paragraph = document.createElement('p')
+console.log('1. : ', paragraph)
+
+
+/**
+ * 2.
+ * TextNode 추가
+ */
+
+paragraph.textContent = '안녕하세요, DOM!'
+console.log('2. : ', paragraph)
